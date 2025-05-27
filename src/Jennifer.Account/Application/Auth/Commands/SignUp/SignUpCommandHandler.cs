f1bf4cf71bae7a191dbec464a810db7419dbe4f0
seokwonhong <h20913@gmail.com>
2025-05-27 16:40:46 +0900

    change SignUpCommandHandler.cs, if user exists, execute verify email code.

--- src/Jennifer.Account/Application/Auth/Commands/SignUp/SignUpCommandHandler.cs
+++ src/Jennifer.Account/Application/Auth/Commands/SignUp/SignUpCommandHandler.cs
@@ -1,24 +1,34 @@
-﻿using Jennifer.Account.Data;
+﻿using eXtensionSharp;
+using Jennifer.Account.Data;
 using Jennifer.Account.Models;
 using Jennifer.Account.Models.Contracts;
 using Jennifer.Account.Session.Abstracts;
 using Jennifer.SharedKernel;
 using Mediator;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 namespace Jennifer.Account.Application.Auth.Commands.SignUp;
 
 internal sealed class SignUpCommandHandler(
     ISessionContext context,
     JenniferDbContext dbContext,
-    IPasswordHasher<User> passwordHasher) : ICommandHandler<SignUpCommand, Result<Guid>>
+    IPasswordHasher<User> passwordHasher,
+    ISender sender) : ICommandHandler<SignUpCommand, Result<Guid>>
 {
     public async ValueTask<Result<Guid>> Handle(SignUpCommand command, CancellationToken cancellationToken)
     {
-        var exists = dbContext.Users.Any(m => m.NormalizedEmail == command.Email.ToUpper());
-        if (exists)
+        var exists = await dbContext.Users.FirstAsync(m => m.NormalizedEmail == command.Email.ToUpper(), cancellationToken: cancellationToken);
+        if (exists.xIsNotEmpty())
         {
-            return Result<Guid>.Failure("email already exists.");
+            switch (exists.EmailConfirmed)
+            {
+                case true:
+                    return Result<Guid>.Failure("Email already exists.");
+                case false:
+                    context.DomainEventPublisher.Enqueue(new EmailVerifyUserDomainEvent(exists));
+                    return Result<Guid>.Success(exists.Id);
+            }
         }
         
         var user = User.Create(context, command.Email, command.UserName, command.PhoneNumber, ENUM_USER_TYPE.CUSTOMER);
