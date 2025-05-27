40a0a38959af83b2d7ec70a8e8b950a99b4409aa
seokwonhong <h20913@gmail.com>
2025-05-27 23:44:50 +0900

    update

---
+++ src/Jennifer.Account/Migrations/20250527144340_change_audit_userid.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Jennifer.Account.Migrations
+{
+    /// <inheritdoc />
+    public partial class change_audit_userid : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "UserID",
+                schema: "account",
+                table: "Audits",
+                newName: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "UserId",
+                schema: "account",
+                table: "Audits",
+                newName: "UserID");
+        }
+    }
+}
